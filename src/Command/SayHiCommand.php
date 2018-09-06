209f73bc882164b417ed4951917f576015912050
Mark Jordan <mjordan@sfu.ca>
2018-09-05 17:49:19 -0700

    Added some application parameters.

--- src/Command/SayHiCommand.php
+++ src/Command/SayHiCommand.php
@@ -5,15 +5,22 @@ namespace App\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Psr\Log\LoggerInterface;
 
 class SayHiCommand extends Command
 {
+    private $params;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(ParameterBagInterface $params, LoggerInterface $logger)
     {
+        $this->params = $params;
+
         // Set output in config/packages/{environment}/monolog.yaml
         $this->logger = $logger;
+
+        // Set in the parameters section of config/services.yaml.
+        $this->fixityHost = $this->params->get('app.fixity.host');
 
         parent::__construct();
     }
@@ -28,7 +35,7 @@ class SayHiCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $now = date(DATE_RFC2822);
-        $output->writeln("Hi, it's $now.");
+        $output->writeln("Hi, it's $now. Your host is ". $this->fixityHost);
 
         $this->logger->info("Hi from Riprap");
     }
