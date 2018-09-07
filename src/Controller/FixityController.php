f16bcd31dd5f48a2df4374bfe95df335aab83e24
Mark Jordan <mjordan@sfu.ca>
2018-09-07 07:58:59 -0700

    Fix on the REST API.

--- src/Controller/FixityController.php
+++ src/Controller/FixityController.php
@@ -2,22 +2,15 @@
 // src/Controller/FixityController.php
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class FixityController
 {
     public function resource($id)
     {
-        $response = new Response();
-
         $data = array('fixity event 1 for resource ' . $id, 'fixity event 2 for resource ' . $id, 'fixity event 3 for resource ' . $id);
-        $content = json_encode($data);
-        $response->setContent($content);
-
-        $response->setStatusCode(Response::HTTP_OK);
-        $response->headers->set('Content-Type', 'application/json');
-
-        $response->send();
+        $response = new JsonResponse($data);
         return $response;
     }
+
 }
