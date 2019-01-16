2290fd7d8ad413f97c8f6fd7f7e561c108d77376
Mark Jordan <mjordan@sfu.ca>
2019-01-16 07:56:05 -0800

    WIP on #31: work on FixityController and PluginPersistToDatabase.

--- src/Controller/FixityController.php
+++ src/Controller/FixityController.php
@@ -2,17 +2,22 @@
 // src/Controller/FixityController.php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\BufferedOutput;
-use Symfony\Component\HttpKernel\KernelInterface;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+// Note that $logger here is independent from the logger that the console command uses.
+use Psr\Log\LoggerInterface;
 
-class FixityController
+// Until we figure out how to define which persist plugin to use in this controller via a
+// single configuration shared between it and the console command, we are limited to using
+// the PluginPersistToDatabase plugin. We could revert to using services.yaml, at least to
+// register persist plugins for this controller, but then we'd have two places to register
+// configuration info.
+use App\Plugin\PluginPersistToDatabase;
+
+class FixityController extends AbstractController
 {
-    public function read(Request $request, ParameterBagInterface $params, KernelInterface $kernel)
+    public function read(Request $request, LoggerInterface $logger)
     {
         // Will be NULL if not present in request.
         $resource_id = $request->headers->get('Resource-ID');
@@ -21,49 +26,33 @@ class FixityController
         $outcome = $request->query->get('outcome');
         // Set default to 'asc' if not in request.
         $sort = !is_null($request->query->get('sort')) ? $request->query->get('sort') : 'asc';
-        // This typecasting makes the derault value of $limit and $offset to be 0.
+        // This typecasting makes the default value of $limit and $offset to be 0.
         $limit = (int) $request->query->get('limit');
         $offset = (int) $request->query->get('offset');
 
         // phpcs:disable
-        // Initial implementation of calling plugin from controller. If you run
+        // If you run
         // curl -v -H 'Resource-ID:http://localhost:8000/mockrepository/rest/10' http://localhost:8000/api/fixity
         // your request will return the fixity event entries in the database for resource 10.
-        // phpcs:ensable
-        $this->params = $params;
-        $this->persistPlugins = $this->params->get('app.plugins.persist');
+        // phpcs:enable
 
-        // @todo: If we allow multiple persist plugins, the last one called determines
-        // the value of $last_digest_for_resource.
-        $this->persistPlugin = $this->persistPlugins[0];
+        $entityManager = $this->getDoctrine()->getManager();
+        // See comment above about hard-coded persist plugin.
+        $this->persist_plugin = new PluginPersistToDatabase(array(), $logger, $entityManager);
+        $events_for_resource = $this->persist_plugin->getEvents(
+            $resource_id,
+            $outcome,
+            $timestamp_start,
+            $timestamp_end,
+            $limit,
+            $offset,
+            $sort
+        );        
 
-        $get_events_plugin_command = new Application($kernel);
-        $get_events_plugin_command->setAutoExit(false);
-
-        $get_events_plugin_input = new ArrayInput(array(
-           'command' => $this->persistPlugin,
-            '--resource_id' => $resource_id,
-            '--timestamp' => '',
-            '--digest_algorithm' => '',
-            '--event_uuid' => '',
-            '--digest_value' => '',
-            '--outcome' => $outcome,
-            '--operation' => 'get_events',
-            '--timestamp_start' => $timestamp_start,
-            '--timestamp_end' => $timestamp_end,
-            '--limit' => $limit,
-            '--offset' => $offset,
-            '--sort' => $sort
-        ));
-        $get_events_plugin_output = new BufferedOutput();
-        $get_events_plugin_return_code = $get_events_plugin_command->run($get_events_plugin_input, $get_events_plugin_output);
-        $events_for_resource = $get_events_plugin_output->fetch();
-
-        // $events_for_resource is a serialized PHP array.
         if (getenv('APP_ENV') == 'test') {
             $response = new JsonResponse(array());
         } else {
-            $response = new JsonResponse(unserialize($events_for_resource));
+            $response = new JsonResponse($events_for_resource);
         }
         return $response;
     }
--- src/Plugin/PluginPersistToDatabase.php
+++ src/Plugin/PluginPersistToDatabase.php
@@ -56,11 +56,51 @@ class PluginPersistToDatabase extends AbstractPersistEventPlugin
         }
     }
 
-    public function getEvents()
+    public function getEvents($resource_id, $outcome, $timestamp_start, $timestamp_end, $limit, $offset, $sort)
     {
         $repository = $this->entityManager->getRepository(FixityCheckEvent::class);
-        // @todo: We'll need to figure out how to pass in all the possible options
-        // from FixityController.php. Maybe just a long list of arguements like
-        // getEvents($resource_id, $outcome, $timestamp_start, $timestamp_end, $limit, $offset, $sort)?
+
+       // If these request query parameters are not present, they are NULL.
+        if (!is_null($resource_id) ||
+            !is_null($timestamp_start) ||
+            !is_null($timestamp_end) ||
+            !is_null($outcome) ||
+            !is_null($sort) ||
+            !is_null($offset) ||
+            !is_null($limit) {
+            $events = $repository->findFixityCheckEventsWithParams(
+                $resource_id,
+                $timestamp_start,
+                $timestamp_end,
+                $outcome,
+                $offset,
+                $limit,
+                $sort
+            );
+        } else {
+            // No request query parameters are present.
+            $events = $repository->findFixityCheckEvents(
+                $resource_id
+            );
+        }
+
+        $event_entries = array();
+        if (count($events)) {
+            foreach ($events as $event) {
+                $event_array = array();
+                $event_array['event_uuid'] = $event->getEventUuid();
+                $event_array['resource_id'] = $event->getResourceId();
+                $event_array['event_type'] = $event->getEventType();
+                $event_array['timestamp'] = $event->getTimestamp();
+                $event_array['digest_algorithm'] = $event->getDigestAlgorithm();
+                $event_array['digest_value'] = $event->getDigestValue();
+                $event_array['event_detail'] = $event->getEventDetail();
+                $event_array['event_outcome'] = $event->getEventOutcome();
+                $event_array['event_outcome_detail_note'] = $event->getEventOutcomeDetailNote();
+                $event_entries[] = $event_array;
+            }
+        }
+
+        return $event_entries;
     }
 }
